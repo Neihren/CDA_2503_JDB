c2eae323120b7388362b6e696a14c9fcec7414a4
Neihren <185206470+Neihren@users.noreply.github.com>
2025-06-04 17:00:30 +0200

    exo Compte

---
+++ 02 - Objet/01_Objet/ClassLibrary_Compte/Compte.cs
@@ -0,0 +1,35 @@
+﻿namespace ClassLibrary_Compte
+{
+    public class Compte
+    {
+        private int numeroUnique;
+        private int nomProprietaire;
+        private decimal solde;
+        private decimal decouvertAutorise;
+
+        public Compte(int numeroUnique, int nomProprietaire, decimal solde, decimal decouvertAutorise)
+        {
+            this.numeroUnique = numeroUnique;
+            this.nomProprietaire = nomProprietaire;
+            this.solde = solde;
+            this.decouvertAutorise = decouvertAutorise;
+        }
+
+        public override string ToString()
+        {
+            return $"Compte[numeroUnique : {this.numeroUnique}, nomProprietaire : {this.nomProprietaire}, solde : {this.solde}, decouvertAutorise : {decouvertAutorise}]";
+        }
+
+        public void Crediter(decimal montantACrediter)
+        {
+            this.solde += montantACrediter;
+        }
+
+        public bool Debiter(decimal montantACrediter)
+        {
+            this.solde -= montantACrediter;
+            return true ;
+        }
+
+    }
+}
