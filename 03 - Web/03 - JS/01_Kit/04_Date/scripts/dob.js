9dc9bb13dbb880090b07e5b39400f51bc4adab79
Neihren <185206470+Neihren@users.noreply.github.com>
2025-07-21 10:50:36 +0200

    Fin exo date de naissance

--- 03 - Web/03 - JS/01_Kit/04_Date/scripts/dob.js
+++ 03 - Web/03 - JS/01_Kit/04_Date/scripts/dob.js
@@ -7,57 +7,26 @@ function calculateAge() {
   const dobLocalDate = dobTimeStamp.toLocaleDateString("fr-FR");
   if (dobTimeStamp <= Date.now()) {
     const age = Math.floor((Date.now() - dobTimeStamp) / 31557600000);
-    info.innerHTML = `<hr>Vous êtes né le <span> ${dobLocalDate} </span> à <span>${dobTimeStamp.toLocaleTimeString("fr-FR")}</span>.<br><br>`
-    info.innerHTML +=`Il s'est écoulé ${age} années depuis votre naissance.`;
+    info.innerHTML = `<hr>Vous êtes né le <span> ${dobLocalDate} </span> à <span>${dobTimeStamp.toLocaleTimeString(
+      "fr-FR"
+    )}</span>.<br><br>`;
+    info.innerHTML += `Il s'est écoulé ${age} années depuis votre naissance.`;
   } else {
     info.innerHTML = "<hr>Sélectionnez une date dans le passé.";
   }
   const signeList = [
-    {
-      nom: "Verseau",
-      date: new Date(dobTimeStamp.getFullYear() + "-01-20"),
-    },
-    {
-      nom: "Poissons",
-      date: new Date(dobTimeStamp.getFullYear() + "-02-19"),
-    },
-    {
-      nom: "Bélier",
-      date: new Date(dobTimeStamp.getFullYear() + "-03-21"),
-    },
-    {
-      nom: "Taureau",
-      date: new Date(dobTimeStamp.getFullYear() + "-04-20"),
-    },
-    {
-      nom: "Gémeaux",
-      date: new Date(dobTimeStamp.getFullYear() + "-05-21"),
-    },
-    {
-      nom: "Cancer",
-      date: new Date(dobTimeStamp.getFullYear() + "-06-21"),
-    },
+    { nom: "Verseau", date: new Date(dobTimeStamp.getFullYear() + "-01-20") },
+    { nom: "Poissons", date: new Date(dobTimeStamp.getFullYear() + "-02-19") },
+    { nom: "Bélier", date: new Date(dobTimeStamp.getFullYear() + "-03-21") },
+    { nom: "Taureau", date: new Date(dobTimeStamp.getFullYear() + "-04-20") },
+    { nom: "Gémeaux", date: new Date(dobTimeStamp.getFullYear() + "-05-21") },
+    { nom: "Cancer", date: new Date(dobTimeStamp.getFullYear() + "-06-21") },
     { nom: "Lion", date: new Date(dobTimeStamp.getFullYear() + "-07-23") },
-    {
-      nom: "Vierge",
-      date: new Date(dobTimeStamp.getFullYear() + "-08-23"),
-    },
-    {
-      nom: "Balance",
-      date: new Date(dobTimeStamp.getFullYear() + "-09-23"),
-    },
-    {
-      nom: "Scorpion",
-      date: new Date(dobTimeStamp.getFullYear() + "-10-23"),
-    },
-    {
-      nom: "Sagittaire",
-      date: new Date(dobTimeStamp.getFullYear() + "-11-21"),
-    },
-    {
-      nom: "Capricorne",
-      date: new Date(dobTimeStamp.getFullYear() + "-12-22"),
-    },
+    { nom: "Vierge", date: new Date(dobTimeStamp.getFullYear() + "-08-23") },
+    { nom: "Balance", date: new Date(dobTimeStamp.getFullYear() + "-09-23") },
+    { nom: "Scorpion", date: new Date(dobTimeStamp.getFullYear() + "-10-23") },
+    { nom: "Sagittaire", date: new Date(dobTimeStamp.getFullYear() + "-11-21") },
+    { nom: "Capricorne", date: new Date(dobTimeStamp.getFullYear() + "-12-22") },
   ];
 
   let userSign = signeList[signeList.length - 1].nom;
