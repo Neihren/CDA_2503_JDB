57a114326534e1518885bc270f37a2a9aaa0e40b
Neihren <185206470+Neihren@users.noreply.github.com>
2025-07-24 16:59:05 +0200

    fonctions

--- 03 - Web/03 - JS/01_Kit/05_Time/scripts/date.js
+++ 03 - Web/03 - JS/01_Kit/05_Time/scripts/date.js
@@ -12,28 +12,42 @@ const userInput = document.getElementById("userInput");
 const spanNBDays = document.getElementById("spanNBDays");
 let now;
 
+function tsToDateString(ts) {
+  const date = `${ts.getFullYear()}-${("0" + (ts.getMonth() + 1)).slice(-2)}-${("0" + ts.getDate()).slice(-2)}`;
+  return date;
+}
+
+function tsToTimeString(ts) {
+  const time = `${("0" + now.getHours()).slice(-2)}:${("0" + now.getMinutes()).slice(-2)}`;
+  return time;
+}
+
+function tsToDays(ts) {
+  const days = Math.floor((now - ts) / 1000 / 60 / 60 / 24 );
+  return days;
+}
+
+function tsToDaysHoursMinutesString(ts) {
+  
+}
+
 function displayDateTime() {
   now = new Date(Date.now());
-  curDate.value = `${now.getFullYear()}-${("0" + (now.getMonth() + 1)).slice(
-    -2
-  )}-${("0" + now.getDate()).slice(-2)}`;
-  curTime.value = `${("0" + now.getHours()).slice(-2)}:${(
-    "0" + now.getMinutes()
-  ).slice(-2)}`;
+  curDate.value = tsToDateString(now);
+  curTime.value  = tsToTimeString(now);
   dateTime.style.display = "block";
-  spanDate.innerText = now.toLocaleDateString();
-  spanHour.innerText = now.toLocaleTimeString();
+  spanDate.textContent = now.toLocaleDateString();
+  spanHour.textContent = now.toLocaleTimeString();
   dateTimeDisplay.style.display = "block";
 }
 
-function calculateInterval() {
+function displayNewInterval() {
     const userDateTime = new Date(Date.parse(userInput.value));
-    const nbDays = Math.floor((now - userDateTime) / 1000 / 60 / 60 / 24 );
-    spanNBDays.innerText = nbDays;
-    spanInterval.innerText = userDateTime.toLocaleDateString("fr-FR");
-    spanInterval.innerText += ` à ${userDateTime.toLocaleTimeString("fr-FR")}` 
+    spanNBDays.textContent = tsToDays(userDateTime);
+    spanInterval.textContent = userDateTime.toLocaleDateString("fr-FR");
+    spanInterval.textContent += ` à ${userDateTime.toLocaleTimeString("fr-FR")}` 
     displayInterval.style.display = "block"
 }
 
 btnDisplayDate.addEventListener("click", displayDateTime);
-btnCalculate.addEventListener("click", calculateInterval);
+btnCalculate.addEventListener("click", displayNewInterval);
