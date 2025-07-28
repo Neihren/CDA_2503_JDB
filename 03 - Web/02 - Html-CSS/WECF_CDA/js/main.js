5a0b64cd0e61485dd4b8c52d7c4e29c893a474d1
Neihren <185206470+Neihren@users.noreply.github.com>
2025-07-28 12:29:52 +0200

    rendu ECF

--- 03 - Web/02 - Html-CSS/WECF_CDA/js/main.js
+++ 03 - Web/02 - Html-CSS/WECF_CDA/js/main.js
@@ -1,2 +1,40 @@
-
 const credits = document.getElementById('credits');
+const validate = document.getElementById('validate');
+const username = document.getElementById('username');
+const result = document.getElementById('result');
+const password1 = document.getElementById('password1');
+const password2 = document.getElementById('password2');
+credits.textContent = "Jessy DE BRITO";
+
+
+function mafonction(event) {
+    const formUsername = username.value.trim();
+    const formPassword1 = password1.value.trim();
+    const formPassword2 = password2.value.trim();
+    if (formUsername.length < 3) {
+        return("Le nom d'utilisateur est trop court (3 caractères minimum)");
+    }
+    else if (formPassword1.length < 12) {
+        return("Le mot de passe est trop court (12 caractères minimum)");
+    }
+    else if (formPassword1 !== formPassword2) {
+        return("Les mots de passe ne correspondent pas");
+    }
+    else {
+        const userNew = {
+            username: formUsername,
+            password: formPassword1
+        }
+        console.log(userNew);
+        document.body.style.backgroundColor = "darkgreen";
+        return("Le formulaire est valide !");
+    }
+}
+
+function display(event) {
+    event.preventDefault();
+    result.textContent = mafonction(event);
+
+}
+
+validate.addEventListener("click", display); // aaaaaaaaaaaa
